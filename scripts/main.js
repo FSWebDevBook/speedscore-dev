fa1fd0f44ffbe471146415b1706b5747424d85ce
Chris Hundhausen <hundhaus@wsu.edu>
2021-04-28 14:23:18 -0700

    Added bottom modebar click functionality * When user clicks on a bottom mode bar button, the mode switches - the  button corresponding to new mode is highlighted * The menu items, app page, and app title change to correspond to mode switch - The global 'mode' variable is updated.

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@
  * GLOBAL VARIABLES TO ASSIST WITH MAINTAINING INTERFACE STATE
 *************************************************************************/
 
-let mode = "FEED"; //The app's current mode
+let mode = "feedMode"; //The app's current mode
 
 /* modeMenuIndices provides the sequence of actual indices into the  "sidemenu-item" DOM element
  * (an unordered list) for each mode. Since the menu items dynamically change based on the mode, 
@@ -19,14 +19,29 @@ let mode = "FEED"; //The app's current mode
  * previous, and next menu item when the user is using the keyboard to cycle through menu items.
  */
 let modeMenuIndices = {
-    "FEED": [0,1,4,5], 
-    "ROUNDS": [2,4,5], 
-    "COURSES": [3,4,5]
+    "feedMode": [0,1,4,5], 
+    "roundsMode": [2,4,5], 
+    "coursesMode": [3,4,5]
 };
 
-let focusIndex = 0; //the index of the current' modes menu item that has the focus. Index 0
+var modeToTitle = {"feedMode": "Activity Feed",
+                   "roundsMode": "My Rounds",
+                   "coursesMode": "Courses",
+                   "loginMode": "Welcome to SpeedScore"};
+
+let focusIndex = 0; //the index of the current mode's menu item that has the focus. Index 0
                     //is the first item, 1 is the second item, and so forth. focusIndex 
-                    //provides an index into the modeMenuIndices arrays
+                    //provides an index into the arrys in the modeMenuIndices object.
+                    //Think of it as a "pointer to a pointer."
+
+/*************************************************************************
+ * SIDE MENU INTERACTION
+ * The following functions implement the functionality of the side menu
+ * for both the visual and accessible keyboard interface.
+ * We use the w3.org  "Navigation Button Example" as a 
+ * specification for implementing the accessible keyboard interface:
+ * https://www.w3.org/TR/wai-aria-practices/examples/menu-button/menu-button-links.html
+*************************************************************************/     
 
 /*************************************************************************
  * @function focusFirstMenuItem
@@ -83,7 +98,7 @@ function focusNextMenuItem() {
  * Open or close the side menu based on current menu state.
  * @param focusItem (optional)
  * Relevant only if we're opening the menu. Indicates which menu item should
- * get the focus ("first" or "last"). If no param is supplied,  If the  button activation should close the menu, then
+ * get the focus ("first" or "last"). If no param is supplied,  
  * the focusItem param is ignored.
  *************************************************************************/
 function toggleSideMenu(focusItem)  {
@@ -128,44 +143,51 @@ function toggleSideMenu(focusItem)  {
 /*************************************************************************
  * @function keyDownMenuBtnFocused
  * @desc 
- * Handle keypress when the menuBtn has the focus. Keyboard semantics
- * taken from https://www.w3.org/TR/wai-aria-practices/examples/menu-button/menu-button-links.html
+ * Handle keypress when the menuBtn has the focus. Permissible keypresses
+ * are the arrow keys, space, and enter. All other keys are ignored.
+ * @param key
+ * The description of the key that was pressed.
  *************************************************************************/
-function keyDownMenuBtnFocused(e) {
-    if (e.code === "ArrowDown" || e.code === "Space" || e.code === "Enter") {
+function keyDownMenuBtnFocused(key) {
+    if (key === "ArrowDown" || key === "Space" || key === "Enter") {
         toggleSideMenu("first");
-    } else if (e.code === "ArrowUp") { //open the menu and focus on last item
+    } else if (key === "ArrowUp") { //open the menu and focus on last item
         toggleSideMenu("last");
     }
 }
 
 
-
 /*************************************************************************
  * @function keyDownMenuItemFocused
  * @desc 
- * Handle keypress when the menu is open and one of the menu items has the focus. Keyboard events
- * taken from https://www.w3.org/TR/wai-aria-practices/examples/menu-button/menu-button-links.html
+ * Handle keypress when the menu is open and one of the menu items has 
+ * focus. Permissible keypresses are Enter, Escape, Up Arrow, Down Arrow,
+ * Home, and End.
+ * @param key
+ * The key that was pressed.
+ * TO DO: The specification says that the user should be able to type 
+ * the first character of a menu item to move to it and that other
+ * keypresses are ignored. This functionality will be left as an
+ * exercise.
  *************************************************************************/
-function keyDownMenuItemFocused(e) {
-    if (e.code == "Enter") { 
+function keyDownMenuItemFocused(key) {
+    if (key == "Enter") { 
         //Choose menu item, close menu, and reset focus to menu btn
         toggleSideMenu();   
         document.getElementById("menuBtn").focus();
         alert("User chose menu item!")
-    } else if (e.code == "Escape") {
+    } else if (key == "Escape") {
         //Close menu without choosing item
         toggleSideMenu(); //Close menu
         document.getElementById("menuBtn").focus();
-        alert("User chose to exit menu without selecting item.")
-    } else if (e.code == "ArrowUp") {
+    } else if (key == "ArrowUp") {
         focusPrevMenuItem();
-    } else if (e.code == "ArrowDown") {
+    } else if (key == "ArrowDown") {
         focusNextMenuItem();
-    } else if (e.code == "Home") {
+    } else if (key == "Home") {
         focusFirstMenuItem();
-    } else if (e.code == "End") {
-        focusPrevMenuItem();
+    } else if (key == "End") {
+        focusLastMenuItem();
     } else {
         //Close menu without choosing item
         toggleSideMenu(); //Close menu
@@ -174,28 +196,88 @@ function keyDownMenuItemFocused(e) {
 }
 
 /*************************************************************************
- * document keydown -- When the user presses a keyboard button, we need
- * to check which item is currently focused and implement the correct
- * functinality. We are using the keyboard mappings specified in the w3c
- * navigatio menu sample:
- * https://www.w3.org/TR/wai-aria-practices/examples/menu-button/menu-button-links.html
+ * MODE BAR INTERACTION
+ * The following functions implement the functionality of the bottom mode
+ * bar for both the visual and accessible keyboard interface.
+ * We use the w3.org  "Example of Tabs with Manual Activiation" as a 
+ * specification for implementing the accessible keyboard interface:
+ * https://www.w3.org/TR/wai-aria-practices/examples/tabs/tabs-2/tabs.html
+*************************************************************************/     
+
+/*************************************************************************
+ * @function switchMode 
+ * @Desc 
+ * Switch from the current mode to a new mode. This entails unhighlighting
+ * the bottom bar btn corresponding to the current mode, highlighting 
+ * the bottom bar btn corresponding to the new mode, updating the global
+ * mode variable, and changing the menu items to reflect the new mode.
+ * @param newMode
+ * The string corresponding to the new mode (feedMode, roundsMode, 
+ * coursesMode, loginMode)
  *************************************************************************/
-document.addEventListener("keydown", function(e) {  
-    if (e.code == "Tab") { //We handle only non-tab events
+function switchMode(newMode) {
+    let prevModeBtn = document.getElementById(mode);
+    let newModeBtn = document.getElementById(newMode);
+    //Switch mode button that is highlighted
+    prevModeBtn.classList.remove("modebar-selected");
+    prevModeBtn.classList.add("modebar-unselected");
+    newModeBtn.classList.remove("modebar-unselected");
+    newModeBtn.classList.add("modebar-selected");
+    //Change page title
+    document.getElementById("appTitle").textContent = modeToTitle[newMode];
+    //Swap out page content
+    document.getElementById(mode + "Page").style.display = "none";
+    document.getElementById(newMode + "Page").style.display = "block";
+    //Change menu items
+    let oldItems = document.getElementsByClassName(mode + "-item");
+    let newItems = document.getElementsByClassName(newMode + "-item");
+    for (let i = 0; i < oldItems.length; ++i) {
+    oldItems[i].style.display = "none";
+    }
+    for (let i = 0; i < newItems.length; ++i) {
+    newItems[i].style.display = "block";
+    }
+    mode = newMode; //Change mode
+}
+
+function keyDownModeBarFocused(key) {
+
+}
+
+//Bind "click" event listener for each mode button to switchMode, passing in the name of
+//the new mode. Note: The following code is equivalent to this:
+// document.getElementById("feedMode").addEventListener("click",() => switchMode("feedMode"));
+// document.getElementById("roundsMode").addEventListener("click",() => switchMode("roundsMode"));
+// document.getElementById("coursesMode").addEventListener("click",() => switchMode("coursesMode"));
+const bottomBtns = document.getElementsByClassName("modebar-btn");
+for (let i = 0; i < bottomBtns.length; ++i) {
+    bottomBtns[i].addEventListener("click",() => switchMode(bottomBtns[i].id));
+}
+
+
+/*************************************************************************
+ * @function Document Keydown Event Handler 
+ * @desc 
+ * When the user presses a keyboard button in the app, we interpret the
+ * keypress based on which user interface element currently has focus. 
+ *************************************************************************/
+ document.addEventListener("keydown", function(e) {  
+    if (e.code == "Tab") { //We allow tab events to pass through.
         return;
     }
     const element = document.activeElement; //The item that currently has focus
     if (element.id === "menuBtn") { 
-        //Keystroke happened when side menu button is focused
-       keyDownMenuBtnFocused(e);
-       e.preventDefault();
+        //Handle keypress when menu button has focus
+       keyDownMenuBtnFocused(e.code);
+      
     } else if (element.getAttribute("role") == "menuitem") { 
-        //Keystroke happened when side menu item is focused
-        keyDownMenuItemFocused(e);
-        e.preventDefault();
+        //Handle key press when side menu item has focus
+        keyDownMenuItemFocused(e.code);
+        
     }
+    //We are handling the interaction here, so prevent default routing.
+    e.preventDefault(); 
 
 });
-
 
   
